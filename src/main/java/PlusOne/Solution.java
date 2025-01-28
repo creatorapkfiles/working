23d4cdb153fe06b8babda2e4b494d6141a7b1f51
Dima <dimashumilow2017@yandex.ru>
2025-01-28 15:42:48 +0300

    Your short summary about the commit

--- src/main/java/PlusOne/Solution.java
+++ src/main/java/PlusOne/Solution.java
@@ -4,5 +4,14 @@ public class Solution {
     public int[] plusOne(int[] digits) {
         return digits;
         // ты должен тут что-то изменить!!!
+        ывап
+                выалпв
+                авпвап
+                        в
+                        апв
+                                ап
+
+                                вапва
+
     }
 }
