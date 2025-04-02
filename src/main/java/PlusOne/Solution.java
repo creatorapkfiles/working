84e760f15017bdfea68f3d9af7d1aa08df539e79
Dmitry Shumilow <dimashumilow2017@yandex.ru>
2025-04-02 12:18:47 +0300

    new line reset new line

--- src/main/java/PlusOne/Solution.java
+++ src/main/java/PlusOne/Solution.java
@@ -3,15 +3,5 @@ package PlusOne;
 public class Solution {
     public int[] plusOne(int[] digits) {
         return digits;
-        // ты должен тут что-то изменить!!!
-        ывап
-                выалпв
-                авпвап
-                        в
-                        апв
-                                ап
-
-                                вапва
-
     }
 }
---
+++ src/test/java/MergeTwoLists/SolutionTest.java
@@ -0,0 +1,16 @@
+package MergeTwoLists;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest extends Assertions {
+
+    @Test
+    void mergeTwoLists() {
+        Solution solution = new Solution();
+        ListNode l1 = new ListNode(1);
+
+    }
+}
