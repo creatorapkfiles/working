872131d41e03a9be0d733e845961d868cdabee72
Dmitry Shumilow <dimashumilow2017@yandex.ru>
2025-10-08 13:17:05 +0300

    solved the problem addBinary

--- src/main/java/addBinary/Solution.java
+++ src/main/java/addBinary/Solution.java
@@ -1,10 +1,30 @@
 package addBinary;
 
+import java.math.BigInteger;
+import java.util.Stack;
+
 public class Solution {
     public String addBinary(String a, String b) {
-        int one = Integer.parseInt(a, 2);
-        int two = Integer.parseInt(b, 2);
-        int sum = one + two;
-        return Integer.toBinaryString(sum);
+        if(a.equals("0") & b.equals("0"))
+            return "0";
+        BigInteger aBig = new BigInteger(a, 2);
+        BigInteger bBig = new BigInteger(b, 2);
+        BigInteger res = aBig.add(bBig);
+
+        return result(res);
+    }
+    private String result(BigInteger res){
+        Stack<BigInteger> stack = new Stack<BigInteger>();
+        StringBuilder sb = new StringBuilder();
+
+        while(res.compareTo(BigInteger.ZERO)>0){
+            stack.push(res.mod(new BigInteger("2")));
+            res = res.divide(BigInteger.TWO);
+        }
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+
+        return sb.toString();
     }
 }
--- src/test/java/addBinary/SolutionTest.java
+++ src/test/java/addBinary/SolutionTest.java
@@ -16,4 +16,14 @@ class SolutionTest extends Assertions {
         Solution s = new Solution();
         assertEquals("10101", s.addBinary("1010", "1011"));
     }
+    @Test
+    void addBinary2() {
+        Solution s = new Solution();
+        assertEquals("0", s.addBinary("0", "0"));
+    }
+    @Test
+    void addBinary3() {
+        Solution s = new Solution();
+        assertEquals("1", s.addBinary("0", "1"));
+    }
 }
